e21fc453e17794ee461be8641d1e2c5158cf4151
Mr159264 <2437022575@qq.com>
2019-07-07 18:00:06 +0800

    代码出错了  写bug 了

--- com.demo.module1/src/Cars.java
+++ com.demo.module1/src/Cars.java
@@ -17,6 +17,6 @@ public class Cars {
 
     public static void main(String[] args) {
         System.out.println("xixi");
-        System.out.printIn("gitHUb 上面修改的");
+        System.out.println("gitHUb 上面修改的");
     }
 }
