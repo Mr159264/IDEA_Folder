c3166254b1df57c541f190567842a1cf494314b4
Mr159264 <2437022575@qq.com>
2019-07-07 19:50:37 +0800

    新建文件夹 和新建一个类

---
+++ com.demo.module1/src/CheakGit/GitForFirst.java
@@ -0,0 +1,12 @@
+package CheakGit;
+
+/**
+ * @version 1.0
+ * @auther zhoutl
+ * @create 2019/07/07/19:49
+ */
+public class GitForFirst {
+    public static void main(String[] args) {
+        System.out.println("我爱你");
+    }
+}
